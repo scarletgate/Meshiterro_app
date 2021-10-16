87361509e80eb0402124027940fe7dd0f616d30d
scarletgate <xiaoyuedu10@gmail.com>
2021-10-16 07:03:07 +0000

    [Finish]4-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9395267d5dad18df6eaac21c63200fdfa83eedbb3b62380396b82b92337ba67937a5824c1bdd3455677dcab8ec402a1022b47903519ac1f4a9f78d497bf0e1d7'
